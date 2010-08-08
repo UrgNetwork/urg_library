b684121681e0b1725cd83d64973cb1d453249343
Satofumi KAMIMURA <devnull@localhost>
2010-08-09 08:19:07 +0900

    fixed urg_index2rad()'s bug

--- current/src/urg_utils.c
+++ current/src/urg_utils.c
@@ -128,7 +128,7 @@ double urg_index2rad(const urg_t *urg, int index)
     }
 
     actual_index = min(max(0, index), urg->last_data_index);
-    step = actual_index - urg->front_data_index;
+    step = actual_index - urg->front_data_index + urg->received_first_index;
     return urg_step2rad(urg, step);
 }
 
