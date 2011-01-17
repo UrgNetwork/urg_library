d565e4cfeb66cd13c6f673e605be483eb3c77b3e
K. Kimoto <kimoto@miksys.com>
2011-01-17 13:35:07 +0900

    windows compile.bat for winsock2

--- current/windowsexe/compile.bat
+++ current/windowsexe/compile.bat
@@ -12,21 +12,21 @@ REM Compile sample utility
 
 cl.exe -c -MD -I../include ../samples/open_urg_sensor.c
 
-REM Compile samples linking with wsock32.lib setupapi.lib with /MD option.
+REM Compile samples linking with ws2_32.lib setupapi.lib with /MD option.
 
-cl.exe /MD -I../include ../samples/sensor_parameter.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/sensor_parameter.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/calculate_xy.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/calculate_xy.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/get_multiecho_intensity.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/get_multiecho_intensity.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/find_port.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/find_port.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/get_distance.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/get_distance.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/get_distance_intensity.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/get_distance_intensity.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/sync_time_stamp.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/sync_time_stamp.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
-cl.exe /MD -I../include ../samples/get_multiecho.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj wsock32.lib setupapi.lib
+cl.exe /MD -I../include ../samples/get_multiecho.c open_urg_sensor.obj urg_sensor.obj urg_utils.obj urg_connection.obj urg_serial.obj urg_serial_utils.obj urg_tcpclient.obj urg_ring_buffer.obj ws2_32.lib setupapi.lib
 
