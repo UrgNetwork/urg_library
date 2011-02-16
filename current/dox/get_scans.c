d9e09ce9eeae5f168a148b3d8b7fc251fbe93a44
Satofumi KAMIMURA <devnull@localhost>
2011-02-16 22:44:28 +0900

    fixed indent

--- current/dox/get_scans.c
+++ current/dox/get_scans.c
@@ -1,16 +1,16 @@
-    // scan_times 回のスキャンデータを取得
+// scan_times 回のスキャンデータを取得
 
-    // urg_start_measurement() 関数でスキャン回数を指定し
-    // urg_get_distance() 関数で指定した回数だけデータを受信する。
+// urg_start_measurement() 関数でスキャン回数を指定し
+// urg_get_distance() 関数で指定した回数だけデータを受信する。
 
-    const int scan_times = 123;
-    int i;
+const int scan_times = 123;
+int i;
 
-    // センサから距離データを取得する。
-    ret = urg_start_measurement(&urg, URG_DISTANCE, scan_times, 0);
-    // \todo check error code
+// センサから距離データを取得する。
+ret = urg_start_measurement(&urg, URG_DISTANCE, scan_times, 0);
+// \todo check error code
 
-    for (i = 0; i < scan_times; ++i) {
-        length_data_size = urg_get_distance(&urg, length_data, NULL);
-        // \todo process length_data array
-    }
+for (i = 0; i < scan_times; ++i) {
+    length_data_size = urg_get_distance(&urg, length_data, NULL);
+    // \todo process length_data array
+ }
