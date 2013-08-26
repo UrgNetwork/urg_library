dcb6701a5264a8bf12bbf71528327a7aa81525c2
jun <devnull@localhost>
2013-08-26 17:35:34 +0900

    ダミーの説明を追加。 スタティックライブラリのみではなく、ダイナミックライブラリも作成するように変更。 linuxでリンク時にエラーになるようなMakefileになっていたのを修正。

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # urgwidget
 
-VERSION = 1.1.1
+VERSION = 1.1.2
 RELEASE_DIR = release
 PACKAGE_EN_DIR = urg_library-$(VERSION)
 PACKAGE_JA_DIR = urg_library_ja-$(VERSION)
--- current/src/urg_tcpclient.c
+++ current/src/urg_tcpclient.c
@@ -4,7 +4,7 @@
 
   \author Katsumi Kimoto
 
-  $Id: urg_tcpclient.c,v d746d6f9127d 2011/05/08 23:10:44 satofumi $
+  $Id$
 */
 
 // http://www.ne.jp/asahi/hishidama/home/tech/lang/socket.html
